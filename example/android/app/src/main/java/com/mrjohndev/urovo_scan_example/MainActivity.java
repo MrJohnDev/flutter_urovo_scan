27d084a91d86a4ff8945d1912bf8f5be2302ab7d
Mr John <38257989+MrJohnDev@users.noreply.github.com>
2022-10-03 10:49:11 +0700

    Remove Kotlin

---
+++ example/android/app/src/main/java/com/mrjohndev/urovo_scan_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mrjohndev.urovo_scan_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- example/android/app/src/main/kotlin/com/mrjohndev/urovo_scan_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mrjohndev.urovo_scan_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
